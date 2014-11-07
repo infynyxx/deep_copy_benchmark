d0d53ea248cc76075d793e364c3f4473d9027fe5
Prajwal Tuladhar <praj@infynyxx.com>
2014-11-07 07:20:46 -0500

    make RunBenchmark compile

--- src/main/java/com/infynyxx/deepCopy/RunBenchmark.java
+++ src/main/java/com/infynyxx/deepCopy/RunBenchmark.java
@@ -7,10 +7,17 @@ import com.google.common.base.Charsets;
 import com.google.common.io.ByteStreams;
 import com.google.common.io.Files;
 import org.openjdk.jmh.Main;
+import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.GenerateMicroBenchmark;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.TimeValue;
+import org.openjdk.jmh.runner.options.VerboseMode;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -21,27 +28,24 @@ import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class RunBenchmark {
-    private static final String TEST = ".*RunBenchmark.*";
-
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper(new JsonFactory());
 
-    public static void main( String[] args ) throws IOException {
-        Main.main(getArguments(TEST, 10, 5000, 10));
-    }
-
-    private static String[] getArguments(String className, int nRuns, int runForMilliseconds, int nThreads) {
-        return new String[]{className,
-                "-i", "" + nRuns,
-                "-r", runForMilliseconds + "ms", // Run time for each iteration. Examples: 100s, 200ms
-                "-t", "" + nThreads,
-                "-wi", "3", // warmup iterations
-                "-v"
-        };
+    public static void main( String[] args ) throws IOException, RunnerException {
+        Options options = new OptionsBuilder()
+                .include(RunBenchmark.class.getSimpleName())
+                .warmupIterations(5)
+                .measurementIterations(5000)
+                .forks(1)
+                .verbosity(VerboseMode.NORMAL)
+                .measurementTime(TimeValue.milliseconds(5000L))
+                .build();
+        new Runner(options).run();
     }
 
     private Map<String, Object> readFile() throws IOException {
         String fileName = "file2.json";
         File file = File.createTempFile(fileName, RunBenchmark.class.getName());
+        file.deleteOnExit();
         InputStream instream = RunBenchmark.class.getResourceAsStream("/" + fileName);
         final OutputStream out = new FileOutputStream(file);
         ByteStreams.copy(instream, out);
@@ -49,14 +53,14 @@ public class RunBenchmark {
         return toMap(input);
     }
 
-    @GenerateMicroBenchmark
+    @Benchmark
     @BenchmarkMode({Mode.Throughput, Mode.AverageTime, Mode.SampleTime, Mode.SingleShotTime})
     @OutputTimeUnit(TimeUnit.MILLISECONDS)
     public void runDeepCopyByteArrayOutputStream() throws IOException {
         DeepCopyByteArrayOutputStream.copy(readFile());
     }
 
-    @GenerateMicroBenchmark
+    @Benchmark
     @BenchmarkMode({Mode.Throughput, Mode.AverageTime, Mode.SampleTime, Mode.SingleShotTime})
     @OutputTimeUnit(TimeUnit.MILLISECONDS)
     public void runDeepCopyKryo() throws IOException {
